788d65f6aa36390acafc061eea86a76bf02d6eee
Dhika Putra <dhikwrr@gmail.com>
2026-05-13 01:41:30 +0700

    feat: add ticket category

---
+++ src/domain/event/events/ticket-category-created.event.ts
@@ -0,0 +1,11 @@
+import { DomainEvent } from "src/domain/shared/domain-event.interface";
+
+export class TicketCategoryCreatedEvent implements DomainEvent {
+    public readonly eventName = "TicketCategoryCreated";
+    public readonly occuredAt = new Date();
+
+    constructor(
+        public readonly eventId: string,
+        public readonly ticketCategoryId: string,
+    ) {}
+}
---
+++ src/domain/event/events/ticket-category-disabled.event.ts
@@ -0,0 +1,11 @@
+import { DomainEvent } from 'src/domain/shared/domain-event.interface';
+
+export class TicketCategoryDisabledEvent implements DomainEvent {
+    public readonly eventName = 'TicketCategoryDisabled';
+    public readonly occurredAt = new Date();
+
+  constructor(
+    public readonly eventId: string,
+    public readonly ticketCategoryId: string,
+  ) {}
+}
